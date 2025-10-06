14b22abec3aa244632a8c0910d1e6b7ce8a02e34
jonathan svec <jotasvec@gmail.com>
2025-10-07 00:16:06 +0200

    updating share icons band

--- article_component/main.js
+++ article_component/main.js
@@ -2,6 +2,5 @@ const sharedButton = document.getElementById('shareButton')
 const shareGroup = document.getElementById('shareGroup')
 sharedButton.addEventListener('click',(e) =>{
     console.log('clicked :>> ', e);
-    shareGroup.innerHTML = "<p>share</p>"
-
+    shareGroup.classList.toggle("hidden");
 })
